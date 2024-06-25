8eb5955fb3dabedd858c4702f4680acc602838bc
Anderson <beltrananderson283@gmail.com>
2024-06-25 18:26:50 -0500

    cambios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,13 +3,13 @@ import Cookies from 'js-cookie';
 
 const clienteAxios = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL || 'https://seepapp.azurewebsites.net',
-    withCredentials: true,
+    withCredentials: true, // Esta línea es crucial para enviar cookies con las solicitudes
 });
 
-// Configurar interceptor para incluir el token en las solicitudes
+// Interceptor para agregar el token a las solicitudes
 clienteAxios.interceptors.request.use(config => {
     const token = Cookies.get('token');
-    console.log("Token obtenido de las cookies:", token); // Agregar este log para depuración
+    console.log("Token obtenido de las cookies:", token); // Log para depuración
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
