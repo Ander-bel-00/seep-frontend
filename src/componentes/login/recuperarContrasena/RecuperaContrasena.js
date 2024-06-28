58a2519fb847954da7411fdbff2d4d09ceb04f6a
Anderson <beltrananderson283@gmail.com>
2024-06-28 01:40:12 -0500

    Mejoras en carga de archivos

--- src/componentes/login/recuperarContrasena/RecuperaContrasena.js
+++ src/componentes/login/recuperarContrasena/RecuperaContrasena.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, Fragment } from "react";
+import React, { useState, useRef, Fragment, useEffect } from "react";
 import clienteAxios from "../../../api/axios";
 import Swal from "sweetalert2";
 import "./css/recuperar.css";
@@ -11,7 +11,14 @@ import { IoHomeSharp } from "react-icons/io5";
 function RecuperaContrasena() {
   const [numeroDocumento, setNumeroDocumento] = useState("");
   const [correoElectronico, setCorreoElectronico] = useState("");
-  const [codigoVerificacion, setCodigoVerificacion] = useState(["", "", "", "", "", ""]);
+  const [codigoVerificacion, setCodigoVerificacion] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
   const [nuevaContrasena, setNuevaContrasena] = useState("");
   const [confirmarContrasena, setConfirmarContrasena] = useState("");
   const [codigoEnviado, setCodigoEnviado] = useState(false);
@@ -19,6 +26,7 @@ function RecuperaContrasena() {
   const [successMessage, setSuccessMessage] = useState("");
   const [showSolicitarCodigo, setShowSolicitarCodigo] = useState(true);
   const [showLoader, setShowLoader] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const codeInputsRefs = useRef([]);
@@ -27,10 +35,13 @@ function RecuperaContrasena() {
     e.preventDefault();
     setShowLoader(true);
     try {
-      const response = await clienteAxios.post("/verificar-correo-electronico", {
-        numero_documento: numeroDocumento,
-        correo_electronico1: correoElectronico,
-      });
+      const response = await clienteAxios.post(
+        "/verificar-correo-electronico",
+        {
+          numero_documento: numeroDocumento,
+          correo_electronico1: correoElectronico,
+        }
+      );
 
       if (!response.data.coincide) {
         setError("El correo electrónico no coincide con el registrado");
@@ -38,15 +49,22 @@ function RecuperaContrasena() {
         return;
       }
 
-      const responseSolicitud = await clienteAxios.post("/solicitar-restablecimiento-contrasena", {
-        numero_documento: numeroDocumento,
-        correo_electronico1: correoElectronico,
-      });
+      const responseSolicitud = await clienteAxios.post(
+        "/solicitar-restablecimiento-contrasena",
+        {
+          numero_documento: numeroDocumento,
+          correo_electronico1: correoElectronico,
+        }
+      );
 
       setSuccessMessage(responseSolicitud.data.mensaje);
       setCodigoEnviado(true);
       setShowSolicitarCodigo(false);
-      Swal.fire("Código enviado", "Se ha enviado un código de verificación a tu correo electrónico", "success");
+      Swal.fire(
+        "Código enviado",
+        "Se ha enviado un código de verificación a tu correo electrónico",
+        "success"
+      );
     } catch (error) {
       console.error("Error de servidor:", error.response);
       setError("Error del servidor: " + JSON.stringify(error.response.data));
@@ -89,6 +107,7 @@ function RecuperaContrasena() {
 
   const handleCambiarContrasena = async (e) => {
     e.preventDefault();
+    setLoading(true); // Activar el estado de carga
     const codigoVerificacionUnido = codigoVerificacion.join("");
 
     if (nuevaContrasena !== confirmarContrasena) {
@@ -107,9 +126,23 @@ function RecuperaContrasena() {
       navigate("/login");
     } catch (error) {
       console.error("Error al cambiar contraseña:", error);
-      setError("Error al cambiar contraseña: " + JSON.stringify(error.response.data));
+      setError(
+        "Error al cambiar contraseña: " + JSON.stringify(error.response.data)
+      );
+    } finally {
+      setLoading(false); // Desactivar el estado de carga
     }
   };
+
+  // Efecto para limpiar errores despues de 5 segundos
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError("");
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   return (
     <div className="login-body">
@@ -122,12 +155,16 @@ function RecuperaContrasena() {
         <div className="recupera-contrasena-container">
           <h1 className="recupera-titulo text-center">Recuperar Contraseña</h1>
 
-          {successMessage && <p className="recupera-mensaje">{successMessage}</p>}
+          {successMessage && (
+            <p className="recupera-mensaje">{successMessage}</p>
+          )}
           {error && <p className="recupera-mensaje">{error}</p>}
           {!codigoEnviado && showSolicitarCodigo ? (
             <div className="recupera-formulario">
               <form onSubmit={handleSolicitarCodigo}>
-                <label htmlFor="numero_documento"><strong>Ingresar Número de Documento</strong></label>
+                <label htmlFor="numero_documento">
+                  <strong>Ingresar Número de Documento</strong>
+                </label>
                 <input
                   className="recupera-input"
                   type="text"
@@ -138,7 +175,9 @@ function RecuperaContrasena() {
                   required
                   placeholder="Número de documento"
                 />
-                <label htmlFor="correo_electronico1"><strong>Ingresar Correo Electrónico</strong></label>
+                <label htmlFor="correo_electronico1">
+                  <strong>Ingresar Correo Electrónico</strong>
+                </label>
                 <input
                   className="recupera-input"
                   type="email"
@@ -188,7 +227,11 @@ function RecuperaContrasena() {
                       <div className="loader"></div>
                     ) : (
                       <button className="recupera-cambio-boton" type="submit">
-                        Cambiar contraseña
+                        {loading ? (
+                          <span className="spinner"></span>
+                        ) : (
+                          "Cambiar contraseña"
+                        )}
                       </button>
                     )}
                   </form>
@@ -204,13 +247,20 @@ function RecuperaContrasena() {
                         type="text"
                         maxLength="1"
                         value={digito}
-                        onChange={(e) => handleChangeCodigo(index, e.target.value)}
-                        ref={(inputRef) => (codeInputsRefs.current[index] = inputRef)}
+                        onChange={(e) =>
+                          handleChangeCodigo(index, e.target.value)
+                        }
+                        ref={(inputRef) =>
+                          (codeInputsRefs.current[index] = inputRef)
+                        }
                       />
                     ))}
                   </div>
                   <div className="flex justify-center items-center mt-2">
-                    <button className="recupera-verificar-boton" onClick={verificarCodigo}>
+                    <button
+                      className="recupera-verificar-boton"
+                      onClick={verificarCodigo}
+                    >
                       Verificar código
                     </button>
                   </div>
@@ -219,11 +269,14 @@ function RecuperaContrasena() {
             </Fragment>
           )}
         </div>
-        <footer className="piePagina">© SENA - Todos los derechos reservados</footer>
+        <footer className="piePagina">
+          © SENA - Todos los derechos reservados
+        </footer>
       </div>
       <div className="inicio-box-main">
         <Link className="link-home" to="/login">
-          <IoHomeSharp className="inline-block" /> <p className="inline-block relative top-1">Ir al Inicio</p>
+          <IoHomeSharp className="inline-block" />{" "}
+          <p className="inline-block relative top-1">Ir al Inicio</p>
         </Link>
       </div>
     </div>
